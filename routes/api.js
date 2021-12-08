adcc70e37caac087e18573856798735d641c336d
alexdmacon <alexdmacon@gmail.com>
2021-12-07 20:46:35 -0600

    starting to write API routes

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+// creates a new Workout instance
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
