ad604e48062586349ee8b22e9de579d6c62db47e
alexdmacon <alexdmacon@gmail.com>
2021-12-11 20:15:55 -0600

    actually got API routes working this time

--- routes/api.js
+++ routes/api.js
@@ -54,7 +54,7 @@ router.get("/api/workouts/range", (req, res) => {
 });
 
 // updates a workout with an exercise
-router.put("/api/workouts/:id", (req, res) => {
+router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     { _id: params.id },
     { $push: { exercises: body } },
@@ -67,7 +67,7 @@ router.put("/api/workouts/:id", (req, res) => {
       res.status(400).json(err);
     });
 });
-
 module.exports = router;
+
 /* > **Important:** Look into using a MongoDB aggregate function to dynamically add up and return the total duration for each workout. Check out the [MongoDB documentation on the $addFields](https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/), the [MongoDB documentation on the $sum operator](https://docs.mongodb.com/manual/reference/operator/aggregation/sum/), and the [Mongoose documentation on aggregate functions](https://mongoosejs.com/docs/api.html#aggregate_Aggregate) to learn how it can be accomplished.
  */
