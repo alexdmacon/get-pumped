e8fee5d92ed8abbdd3594149bb5f8fad213ecfe2
alexdmacon <alexdmacon@gmail.com>
2021-12-11 19:45:24 -0600

    fixed API routes with aggregate function. New exercise submit not working.

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,6 @@ const Workout = require("../models/workout.js");
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then((dbWorkout) => {
-
       console.log(dbWorkout);
 
       res.json(dbWorkout);
@@ -16,9 +15,16 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-// fetches workouts
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
     .sort({ date: -1 })
     .then((dbWorkout) => {
       res.json(dbWorkout);
@@ -28,39 +34,39 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-// gets workouts from db for display on "/stats" page. Is this where I would dynamically populate duration?
 router.get("/api/workouts/range", (req, res) => {
-	Workout.find({}, null, { sort: { day: 1 } })
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+        totalWeight: { $sum: "$exercises.weight" },
+      },
+    },
+  ])
     .limit(7)
-
-		.then((dbWorkout) => {
-			res.json(dbWorkout);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
-
 
 // updates a workout with an exercise
 router.put("/api/workouts/:id", (req, res) => {
-	var workoutID = req.params.id;
-	Workout.create(req.body)
-		.then(({ _id }) =>
-			Workout.findOneAndUpdate(
-				{ _id: workoutID },
-				{ $push: { exercises: _id } },
-				{ new: true }
-			)
-		)
-		.then((dbWorkout) => {
-			res.json(dbWorkout);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+  Workout.findByIdAndUpdate(
+    { _id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
-
 
 module.exports = router;
 /* > **Important:** Look into using a MongoDB aggregate function to dynamically add up and return the total duration for each workout. Check out the [MongoDB documentation on the $addFields](https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/), the [MongoDB documentation on the $sum operator](https://docs.mongodb.com/manual/reference/operator/aggregation/sum/), and the [Mongoose documentation on aggregate functions](https://mongoosejs.com/docs/api.html#aggregate_Aggregate) to learn how it can be accomplished.
